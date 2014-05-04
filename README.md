112130c1883dbeb88c807698e6ae6d431fd3c0ae
Mitchell Plamann <mitchell.plamann@gmail.com>
2014-05-04 14:23:24 -0400

    The program seems to be mostly working.

---
+++ README.md
@@ -0,0 +1,8 @@
+Interactive-graph
+=================
+
+This is mostly a project that I made to try out a few different
+Haskell libraries (attoparsec and threepenny-gui). It takes in a
+specification for a graph and displays a HTML interface using
+threepenny-gui that allows you to step through the graph node by
+node. It makes the most sense for flowcharts or DFAs.
--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,42 @@
 module Main where
 
 import Control.Monad
+import qualified Graphics.UI.Threepenny as UI
+import Graphics.UI.Threepenny.Core
+import System.Environment
 
 import Graph
-import GraphTests
-
-doNode (Node label desc neighbors) = do
-  putStrLn desc
-  unless (null neighbors) $ do
-    putStrLn "Options:"
-    mapM_ (putStrLn . ("  " ++ ) . fst) neighbors
-    putStr "> "
-    choice <- getLine
-    doNode (snd $ neighbors !! read choice)
 
 main = do
-  file <- readFile "testGraph.flw"
-  doNode (loadFromString file)
+  args <- getArgs
+  case args of
+    [file] -> do
+      startGUI defaultConfig {
+        tpPort = Just 9000
+        , tpStatic = Just "../wwwroot"
+        } (setup file)
+    _ -> getProgName >>= \p -> putStrLn ("Usage: " ++ p ++ " file")
 
-runTest = do
-  putStrLn $ test "mkGraph" testMkGraph
-  putStrLn $ test "loadFromString" testLoadFromString
+doNode (Node _ desc neighbors) text div = do
+  element text # set UI.text desc
+  set children [] (div)
+  forM_ neighbors $ \(str, node) -> do
+    button <- UI.button # set UI.text str
+    on UI.click button $ const $ do
+      doNode node text div
+    div #+ [element button]
+
+setup filename window = do
+  return window # set UI.title "Graph Explorer"
+  text <- UI.p # set UI.text "hi" # set UI.id_ "responseText"
+  div <- UI.div
+  button <- UI.button # set UI.text "Click me!"
+  element div #+ [element button]
+  getBody window #+ [element text, element div]
+  on UI.click button $ const $ do
+    element button # set UI.text "I have been clicked!"
+    button2 <- UI.button # set UI.text "Who, me?"
+    element div #+ [element button2]
+    element text # set UI.text "Button was clicked!"
+  file <- liftIO $ readFile filename
+  doNode (loadFromString file) text (return div)
